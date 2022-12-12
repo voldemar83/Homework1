770c47e3eddace8073a4ae7c90a40dfcf6197807
Vladimir83 <vovaf@bk.ru>
2022-12-12 12:38:45 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число:");
+int num1 = int.Parse(Console.ReadLine() ?? "");
+Console.WriteLine("Введите второе число:");
+int num2 = int.Parse(Console.ReadLine() ?? "");
+if (num1 > num2)
+{
+    Console.WriteLine($"max = {num1}");
+}
+else if (num2 > num1)
+{
+    Console.WriteLine($"max = {num2}");
+}
+else
+{
+    Console.WriteLine("Числа равны");
+}
