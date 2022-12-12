1d83dcc379a66c25bf4aaeab674ec59a71d49571
Vladimir83 <vovaf@bk.ru>
2022-12-12 12:53:51 +0300

    task8

---
+++ task8/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число:");
+int num = int.Parse(Console.ReadLine() ?? "");
+int count = 2;
+if (num == 1)
+{
+    Console.WriteLine("Четных чисел нет");
+}
+while (count <= num)
+{
+    Console.Write($"{count} ");
+    count +=2;
+}
+Console.WriteLine();
