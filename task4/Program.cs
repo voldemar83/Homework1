9ca821e1a9c24173ee641942c53010f86988cd77
Vladimir83 <vovaf@bk.ru>
2022-12-12 12:44:15 +0300

    task4

---
+++ task4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите первое число:");
+int num1 = int.Parse(Console.ReadLine() ?? "");
+Console.WriteLine("Введите второе число:");
+int num2 = int.Parse(Console.ReadLine() ?? "");
+Console.WriteLine("Введите третье число:");
+int num3 = int.Parse(Console.ReadLine() ?? "");
+int max = num1;
+if (num2 > max) max = num2;
+if (num3 > max) max = num3;
+Console.WriteLine();
+Console.WriteLine($"max = {max}");
