503f7031c69b21c2b4a8e72f584aa0892b23c16a
Vladimir83 <vovaf@bk.ru>
2022-12-12 12:49:57 +0300

    task6

---
+++ task6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число:");
+int num = int.Parse(Console.ReadLine() ?? "");
+if (num % 2 == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число нечетное");
+}
